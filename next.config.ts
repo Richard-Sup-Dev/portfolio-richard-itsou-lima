52cff8c8db18313f86ab307b4f7b1bf297bcbead
Richard <natsunokill188@gmail.com>
2026-01-15 15:25:25 -0300

    fix: remove metadataBase inválido do next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  metadataBase: new URL('https://portfolio-richard-itsou-lima-cxvrigrms.vercel.app'),
 };
 
 export default nextConfig;
