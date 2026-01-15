b0332f474af3b002bba56be88169c98fd879cb68
Richard <natsunokill188@gmail.com>
2026-01-15 00:04:49 -0300

    fix: TypeScript null check in Header.tsx

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  metadataBase: new URL('https://portfolio-richard-itsou-lima-cxvrigrms.vercel.app'),
 };
 
 export default nextConfig;
